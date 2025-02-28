73b776967aff11bce5d829ff6d460fbda3d7e5a1
atikur-24 <atikurrahman7223@gmail.com>
2025-02-28 19:19:19 +0600

    Create NA plus advantage section

---
+++ public/data/advantage.js
@@ -0,0 +1,70 @@
+import {
+  FaBook,
+  FaCommentDots,
+  FaDesktop,
+  FaFilePen,
+  FaLightbulb,
+  FaPersonCircleQuestion,
+} from "react-icons/fa6";
+
+export const advantages = [
+  {
+    id: 1,
+    title: "Teaching Methodology",
+    icon: <FaDesktop />,
+    data: [
+      "Concept & application based learning",
+      "Interactive & engaging lectures",
+      "Integrated approach focusing on competitive & school exams",
+    ],
+  },
+  {
+    id: 2,
+    title: "Study Material",
+    icon: <FaBook />,
+    data: [
+      "Well-researched & analysed",
+      "Comprehensive & meticulously designed",
+      "Based on latest syllabus & exam pattern",
+    ],
+  },
+  {
+    id: 3,
+    title: "Regular Tests & Assessments",
+    icon: <FaFilePen />,
+    data: [
+      "Detailed test analysis with error analysis report",
+      "Simulated tests based on latest exam pattern",
+      "Compete at national level with All India Aakash Test Series",
+    ],
+  },
+  {
+    id: 4,
+    title: "Doubt Clearing System",
+    icon: <FaPersonCircleQuestion />,
+    data: [
+      "One-to-one interactive session with subject experts",
+      "Detailed & simplified solutions",
+      "Video solutions for instant doubt resolution",
+    ],
+  },
+  {
+    id: 5,
+    title: "Feedback System",
+    icon: <FaCommentDots />,
+    data: [
+      "Timely updates on performance & attendance",
+      "Query/ grievance redressal",
+    ],
+  },
+  {
+    id: 6,
+    title: "Omni-channel Learning",
+    icon: <FaLightbulb />,
+    data: [
+      "100% digitised classrooms across all branches or centres",
+      "Hybrid classrooms offering the best of classroom & online learning",
+      "Digital Product (Recorded Video Lectures, e-Content, e-Test Papers/Series)",
+    ],
+  },
+];
---
+++ src/app/(main)/_components/NAAdvantage/NAAdvantage.jsx
@@ -0,0 +1,41 @@
+import Container from "@/components/ui/Container";
+import SectionTitle from "@/components/ui/SectionTitle";
+import React from "react";
+import { RxDotFilled } from "react-icons/rx";
+import { advantages } from "../../../../../public/data/advantage";
+
+const NAAdvantage = () => {
+  return (
+    <div className="bg-[#F8F8F8]">
+      <Container>
+        <SectionTitle
+          title="NonAcademy Plus Advantage"
+          subtitle="Stay ahead with all-round performance in your chosen stream"
+        />
+
+        <div className="mt-5 grid grid-cols-1 gap-4 md:mt-9 md:grid-cols-2 md:gap-6 lg:grid-cols-3 lg:gap-7">
+          {advantages?.map((advantage) => (
+            <div
+              key={advantage.id}
+              className="min-h-[250px] rounded-lg bg-white p-5 shadow-[0_1px_8px_0_rgba(0,0,0,0.08)] md:p-7"
+            >
+              <div className="mb-3 flex items-center gap-3 text-lg font-semibold md:mb-4 md:text-xl">
+                <div>{advantage.icon}</div>
+                <h6 className="text-nad-gray-7">{advantage.title}</h6>
+              </div>
+              <ul className="space-y-2 text-sm text-gray-700">
+                {advantage.data?.map((item, idx) => (
+                  <li key={idx}>
+                    <RxDotFilled className="inline-flex text-xs" /> {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default NAAdvantage;
