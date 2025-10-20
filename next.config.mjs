276d4cb8922a5a0c50070e941b12954d9d7131ae
Mainulhasan05 <mdrifatbd5@gmail.com>
2025-10-20 23:51:58 +0600

    adding all

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     // dev backend url
     // API_URL: "http://localhost:5000/api",
     // API_URL: "https://nonacademy.mainulhasan05.xyz/api",
-    API_URL: "https://na-api.nonacademy.net/api",
+    API_URL: "https://oa-api.originacademia.com/api",
     API_URL_V2: "https://arif3.com/api",
     // NEXT_PUBLIC_PIXEL_ID: "840544851499439",
   },
